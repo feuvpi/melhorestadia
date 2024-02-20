f8d127e35378a56844deb57b44f174d0fea98d1f
feuvpi <fredvpg@gmail.com>
2024-02-20 00:42:35 -0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	// @ts-nocheck
 
 	import ArticleCard from '../lib/components/Card.svelte';
+	import Grid from '../lib/components/Grid.svelte';
 
 	// export async function load() {
 	// 	const posts = blogMetadata;
@@ -37,16 +38,37 @@
 
 <body class="bg-white font-family-karla">
 	<!-- Top Bar Nav -->
-	<nav class="w-full py-4 bg-emerald-800 shadow">
-		<div class="w-full container mx-auto flex flex-wrap items-center justify-between">
-			<nav>
-				<ul
-					class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline"
-				>
-					<li><a class="hover:text-gray-200 hover:underline px-4" href="#">Shop</a></li>
-					<li><a class="hover:text-gray-200 hover:underline px-4" href="#">About</a></li>
-				</ul>
-			</nav>
+
+	<div
+		class="flex flex-col items-center"
+		style="background-image: url('data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 1920 1080%22%3E%3Cg transform=%22rotate(170 960 540) translate(-281.77499944328065 -158.49843718684542) scale(1.293515624420084)%22%3E%3Crect width=%221920%22 height=%221080%22 fill=%22rgb(142, 217, 121)%22%3E%3C/rect%3E%3Cg transform=%22translate(0, 0)%22%3E%3Cpath fill=%22rgb(107, 194, 127)%22 fill-opacity=%221%22 d=%22M0,264.707L45.714,262.556C91.429,260.405,182.857,256.103,274.286,255.436C365.714,254.769,457.143,257.737,548.571,258.071C640,258.405,731.429,256.104,822.857,227.387C914.286,198.67,1005.714,143.537,1097.143,138.881C1188.571,134.225,1280,180.045,1371.429,166.002C1462.857,151.959,1554.286,78.052,1645.714,73.86C1737.143,69.669,1828.571,135.193,1874.286,167.955L1920,200.718L1920,1080L1874.286,1080C1828.571,1080,1737.143,1080,1645.714,1080C1554.286,1080,1462.857,1080,1371.429,1080C1280,1080,1188.571,1080,1097.143,1080C1005.714,1080,914.286,1080,822.857,1080C731.429,1080,640,1080,548.571,1080C457.143,1080,365.714,1080,274.286,1080C182.857,1080,91.429,1080,45.714,1080L0,1080Z%22%3E%3C/path%3E%3C/g%3E%3Cg transform=%22translate(0, 270)%22%3E%3Cpath fill=%22rgb(71, 171, 132)%22 fill-opacity=%221%22 d=%22M0,102.07L45.714,88.075C91.429,74.081,182.857,46.091,274.286,60.539C365.714,74.987,457.143,131.872,548.571,142.129C640,152.386,731.429,116.015,822.857,97.811C914.286,79.606,1005.714,79.568,1097.143,105.956C1188.571,132.344,1280,185.158,1371.429,213.523C1462.857,241.888,1554.286,245.804,1645.714,213.506C1737.143,181.208,1828.571,112.697,1874.286,78.441L1920,44.186L1920,810L1874.286,810C1828.571,810,1737.143,810,1645.714,810C1554.286,810,1462.857,810,1371.429,810C1280,810,1188.571,810,1097.143,810C1005.714,810,914.286,810,822.857,810C731.429,810,640,810,548.571,810C457.143,810,365.714,810,274.286,810C182.857,810,91.429,810,45.714,810L0,810Z%22%3E%3C/path%3E%3C/g%3E%3Cg transform=%22translate(0, 540)%22%3E%3Cpath fill=%22rgb(36, 147, 138)%22 fill-opacity=%221%22 d=%22M0,80.341L45.714,100.73C91.429,121.12,182.857,161.899,274.286,190.747C365.714,219.596,457.143,236.513,548.571,225.386C640,214.259,731.429,175.089,822.857,135.235C914.286,95.38,1005.714,54.842,1097.143,32.523C1188.571,10.205,1280,6.106,1371.429,30.809C1462.857,55.513,1554.286,109.018,1645.714,125.837C1737.143,142.655,1828.571,122.785,1874.286,112.85L1920,102.916L1920,540L1874.286,540C1828.571,540,1737.143,540,1645.714,540C1554.286,540,1462.857,540,1371.429,540C1280,540,1188.571,540,1097.143,540C1005.714,540,914.286,540,822.857,540C731.429,540,640,540,548.571,540C457.143,540,365.714,540,274.286,540C182.857,540,91.429,540,45.714,540L0,540Z%22%3E%3C/path%3E%3C/g%3E%3Cg transform=%22translate(0, 810)%22%3E%3Cpath fill=%22rgb(0, 124, 143)%22 fill-opacity=%221%22 d=%22M0,38.612L45.714,43.474C91.429,48.336,182.857,58.059,274.286,64.956C365.714,71.853,457.143,75.924,548.571,99.792C640,123.66,731.429,167.326,822.857,197.063C914.286,226.799,1005.714,242.607,1097.143,252.089C1188.571,261.571,1280,264.727,1371.429,246.468C1462.857,228.209,1554.286,188.536,1645.714,156.971C1737.143,125.406,1828.571,101.95,1874.286,90.222L1920,78.494L1920,270L1874.286,270C1828.571,270,1737.143,270,1645.714,270C1554.286,270,1462.857,270,1371.429,270C1280,270,1188.571,270,1097.143,270C1005.714,270,914.286,270,822.857,270C731.429,270,640,270,548.571,270C457.143,270,365.714,270,274.286,270C182.857,270,91.429,270,45.714,270L0,270Z%22%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E'); background-size: cover;"
+	>
+		<a class="flex items-center justify-center" href="#">
+			<img src="/src/lib/shared/logo_1.png" alt="Logo" class="w-88 h-28" />
+		</a>
+	</div>
+
+	<nav class="w-full border-t border-b bg-gray-100">
+		<div class="block sm:hidden">
+			<!-- <a
+                href="#"
+                class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+                @click="open = !open"
+            >
+                Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
+            </a> -->
+		</div>
+		<div class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+			<div
+				class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2"
+			>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
+				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+			</div>
 		</div>
 	</nav>
 
@@ -139,45 +161,9 @@
 		</div>
 	</div>
 
-	<!-- Text Header -->
-	<header class="w-full container mx-auto">
-		<div class="flex flex-col items-center py-12">
-			<a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
-				LUGARES NA NATUREZA
-			</a>
-			<p class="text-lg text-gray-600">
-				Encontre as melhores e mais baratas cabines e chales do Brasil. Alugue a melhor acomodação
-				em qualquer lugar do Brasil!
-			</p>
-		</div>
-	</header>
-
 	<!-- Topic Nav -->
 	<!-- <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }"></nav> -->
-	<nav class="w-full py-4 border-t border-b bg-gray-100">
-		<div class="block sm:hidden">
-			<!-- <a
-                href="#"
-                class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
-                @click="open = !open"
-            >
-                Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
-            </a> -->
-		</div>
-		<div class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-			<div
-				class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2"
-			>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
-				<a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
-			</div>
-		</div>
-	</nav>
-
+	<Grid />
 	<div class="container mx-auto flex flex-wrap py-6">
 		<!-- Posts Section -->
 
@@ -208,7 +194,7 @@
 		</section>
 
 		<!-- Sidebar Section -->
-		<aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+		<!-- <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 			<div class="w-full bg-white shadow flex flex-col my-4 p-6">
 				<p class="text-xl font-semibold pb-5">About Us</p>
 				<p class="pb-2">
@@ -270,7 +256,7 @@
 					<i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
 				</a>
 			</div>
-		</aside>
+		</aside> -->
 	</div>
 
 	<footer class="w-full border-t bg-white pb-12">
