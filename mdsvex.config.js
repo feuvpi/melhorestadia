3011feb5e982f11bad1dd15b672572c248cedf30
feuvpi <fredvpg@gmail.com>
2024-02-27 23:49:39 -0300

    .

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -7,7 +7,7 @@ const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
 const config = defineConfig({
     extensions: ['.md', '.svx'],
     smartypants: { dashes: 'oldschool' },
-    layout: { blog: path.join(dirname, './src/routes/posts/post.svelte') }
+    // layout: { blog: path.join(dirname, './src/routes/posts/post.svelte') }
 });
 
 export default config;
